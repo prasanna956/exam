2ec0b9385ffccff59a1d481bd115e93566cd83a4
prasannachore <prasannachore@gmail.com>
2022-12-08 17:39:55 +0530

    first

--- menu.py
+++ menu.py
@@ -3,18 +3,19 @@ p=120
 t=3
 r=2.5
 n=2
-f=True
-while(f==True):
-    ch= int(input("Enter the Choice:"))
-    if ch== 1:  
-       asi=p*(1+ (r*t))
-       print("\n Simple Interest = ",asi)
-       f=False
-    elif ch== 2:  
-        aci=p*((1+ r/n) ** (n*t))
-        print("\n Compound Interest = ",aci)
-        f=False
-    else:
-        print("Incorrect")
-        f=False
+f=1
+while(f==1):
+    #ch= int(input("Enter the Choice:"))
+    #if ch== 1:  
+    asi=p*(1+ (r*t))
+    print("\n Simple Interest = ",asi)
+    
+    #elif ch== 2:  
+    aci=p*((1+ r/n) ** (n*t))
+    print("\n Compound Interest = ",aci)
+        
+    #else:
+    #print("Incorrect")
+    f=f-1
+        
     
